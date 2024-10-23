318457a6fed70971c8bdf73f3cf057dd51071ebb
JimGu <gwt200407@gmail.com>
2024-10-23 14:36:01 +0800

    Add static configuration & training script

--- train.py
+++ train.py
@@ -1,11 +1,46 @@
+import torch
+
+from torch import nn
 from torch.utils.data import DataLoader
 
-from model import PointDecoder
+from config import settings
 from data import PseCoDataset
+from model import PointDecoderCNN
+from pretrained_models.segment_anything.build_sam import build_sam_vit_h
 
-img_dir = "data/train_data/images"
-gt_dir = "data/train_data/ground_truth"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-dataset = PseCoDataset(img_dir, gt_dir)
-data = DataLoader(dataset, batch_size=4)
-model = PointDecoder()
+dataset = PseCoDataset(settings.TRAIN_IMAGE, settings.TRAIN_GT)
+sam = build_sam_vit_h().to(device)
+model = PointDecoderCNN(sam).to(device)
+loss_fn = nn.MSELoss(reduction="none")
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+model.pd.load_state_dict(torch.load(settings.MODEL_POINT_DECODER, weights_only=True))
+
+
+def train(batch_size: int = 32, epochs: int = 100):
+    model.train()
+    data = DataLoader(dataset, batch_size=batch_size)
+
+    for i in range(1, epochs + 1):
+        train_loss = 0
+
+        for image, points, heatmap in data:
+            pred_heatmap = model(image, points)
+
+            optimizer.zero_grad()
+            loss = loss_fn(pred_heatmap, heatmap)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss / len(data)
+
+        print(f"[Epoch {i} / {epochs}] Train loss: {train_loss}.")
+
+        if not i % 20 or i == epochs:
+            print(f"Model saved at epoch {i}.")
+            torch.save(model.state_dict(), settings.MODEL_POINT_DECODER_CNN)
+
+
+train()
