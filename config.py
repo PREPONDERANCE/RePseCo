3ae9c7c2313f687647692943a31be3db86d42a05
JimGu <gwt200407@gmail.com>
2024-10-23 17:42:02 +0800

    Fix typo in config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Settings:
-    _base_model_route = "params/{}/{}"
-    _base_data_route = "data/{}"
+    _base_model_route = "params/{}"
+    _base_data_route = "data/{}/{}"
 
     MODEL_POINT_DECODER = _base_model_route.format("point_decoder_vith.pth")
-    MODEL_POINT_DECODER_CNN = _base_data_route.format("point_decoder_cnn_vith.pth")
+    MODEL_POINT_DECODER_CNN = _base_model_route.format("point_decoder_cnn_vith.pth")
     MODEL_CLIP_ZERO_SHOT = _base_model_route.format("MLP_small_box_w1_zeroshot.tar")
 
     TRAIN_IMAGE = _base_data_route.format("train", "images")
